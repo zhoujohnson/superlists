50730848a04e41d4c50949ba5065a02fd9d80468
zhoujohnson <zhoujohnson@icloud.com>
2020-01-13 00:48:23 +0800

    no message

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -7,10 +7,13 @@ app_name ='polls'  # 注册命名空间
 
 
 urlpatterns = [
-    path('',views.index,name='index'),
-    # path('',views.detail,name='detail'),
-    path('<int:question_id>/',views.detail,name ='detail'),
-    path('<int:question_id>/results/',views.results,name='results'),
-    path('<int:question_id>/vote/',views.vote,name='vote'),
+    # path('',views.index,name='index'),
+    # path('<int:question_id>/',views.detail,name ='detail'),
+    # path('<int:question_id>/results/',views.results,name='results'),
+    # path('<int:question_id>/vote/',views.vote,name='vote'),
+    path('',views.IndexView.as_view(),name ='index'),
+    path('<int:pk>/',views.DetailView.as_view(),name='detail'),
+    path('<int:pk>/',views.ResultsView.as_view(),name='results'),
+
 
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -6,26 +6,51 @@ from .models import Question,Choice
 from django.urls import reverse
 # Create your views here.
 # 编写视图
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # template =loader.get_template('polls/index.html')
-    context ={'latest_question_list':latest_question_list,}
-    # return HttpResponse(template.render(context, request))
-    return render(request,'polls/index.html',context)
+from django.views import generic
 
-def detail(request,question_id):
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+
+
+
+
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     # template =loader.get_template('polls/index.html')
+#     context ={'latest_question_list':latest_question_list,}
+#     # return HttpResponse(template.render(context, request))
+#     return render(request,'polls/index.html',context)
+
+# def detail(request,question_id):
     # try:
     #     question = Question.objects.get(pk=question_id)
     # except Question.DoesNotExist:
     #     raise Http404("Question does not exist")
     # return render(request,'polls/detail.html',{'question':question})
-    question = get_object_or_404(Question,pk=question_id)
-    return render(request,'polls/detail.html',{'question':question})
-
-
-def results(request,question_id):
-    response = "You're looking at the result of question %s"
-    return HttpResponse(response% question_id)
+#     question = get_object_or_404(Question,pk=question_id)
+#     return render(request,'polls/detail.html',{'question':question})
+#
+#
+# def results(request,question_id):
+#     response = "You're looking at the result of question %s"
+#     return HttpResponse(response% question_id)
 
 def vote(request,question_id):
     question = get_object_or_404(Question,pk=question_id)
@@ -38,8 +63,8 @@ def vote(request,question_id):
         selected_choice.votes +=1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
-
-def results(request,question_id):
-    question = get_object_or_404(Question,pk=question_id)
-    return render(request,'polls/results.html',{'question':question})
+#
+# def results(request,question_id):
+#     question = get_object_or_404(Question,pk=question_id)
+#     return render(request,'polls/results.html',{'question':question})
 
